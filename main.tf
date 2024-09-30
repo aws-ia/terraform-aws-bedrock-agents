b5711cf7e40499ee75816112ff7ccad908c56088
Alexa Perlov <aperlov@amazon.com>
2024-09-30 17:03:37 -0400

    running pre-commit

--- main.tf
+++ main.tf
@@ -169,3 +169,291 @@ resource "aws_bedrockagent_data_source" "knowledge_base_ds" {
     }
   }
 }
+
+# - Knowledge Base Default OpenSearch -
+
+resource "awscc_bedrock_knowledge_base" "knowledge_base_default" {
+  count       = var.create_default_kb ? 1 : 0
+  name        = var.kb_name
+  description = var.kb_description
+  role_arn    = var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn
+
+  storage_configuration = {
+    type = "OPENSEARCH_SERVERLESS"
+    opensearch_serverless_configuration = {
+      collection_arn    = awscc_opensearchserverless_collection.default_collection.arn
+      vector_index_name = var.vector_index_name # bedrock-knowledge-base-default-index
+      field_mapping = {
+        metadata_field = var.metadata_field # "AMAZON_BEDROCK_METADATA
+        text_field     = var.text_field     # "AMAZON_BEDROCK_TEXT_CHUNK"
+        vector_field   = var.vector_field   # "bedrock-knowledge-base-default-vector"
+      }
+    }
+  }
+  knowledge_base_configuration = {
+    type = "VECTOR"
+    vector_knowledge_base_configuration = {
+      embedding_model_arn = var.kb_embedding_model_arn # "arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-embed-text-v1"
+    }
+  }
+}
+
+# – Existing KBs –
+
+# - Mongo –
+resource "awscc_bedrock_knowledge_base" "knowledge_base_mongo" {
+  count       = var.create_mongo_config ? 1 : 0
+  name        = var.kb_name
+  description = var.kb_description
+  role_arn    = var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn
+  tags        = var.kb_tags
+
+  storage_configuration = {
+    type = var.kb_storage_type
+
+    mongo_db_atlas_configuration = {
+      collection_name        = var.collection_name
+      credentials_secret_arn = var.credentials_secret_arn
+      database_name          = var.database_name
+      endpoint               = var.endpoint
+      vector_index_name      = var.vector_index_name
+      field_mapping = {
+        metadata_field = var.metadata_field
+        text_field     = var.text_field
+        vector_field   = var.vector_field
+      }
+      endpoint_service_name = var.endpoint_service_name
+    }
+  }
+  knowledge_base_configuration = {
+    type = var.kb_type
+    vector_knowledge_base_configuration = {
+      embedding_model_arn = var.kb_embedding_model_arn
+    }
+  }
+}
+
+# – OpenSearch –
+resource "awscc_bedrock_knowledge_base" "knowledge_base_opensearch" {
+  count       = var.create_opensearch_config ? 1 : 0
+  name        = var.kb_name
+  description = var.kb_description
+  role_arn    = var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn
+  tags        = var.kb_tags
+
+  storage_configuration = {
+    type = var.kb_storage_type
+    opensearch_serverless_configuration = {
+      collection_arn    = var.collection_arn
+      vector_index_name = var.vector_index_name
+      field_mapping = {
+        metadata_field = var.metadata_field
+        text_field     = var.text_field
+        vector_field   = var.vector_field
+      }
+    }
+  }
+  knowledge_base_configuration = {
+    type = var.kb_type
+    vector_knowledge_base_configuration = {
+      embedding_model_arn = var.kb_embedding_model_arn
+    }
+  }
+}
+
+# – Pinecone –
+resource "awscc_bedrock_knowledge_base" "knowledge_base_pinecone" {
+  count       = var.create_pinecone_config ? 1 : 0
+  name        = var.kb_name
+  description = var.kb_description
+  role_arn    = var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn
+  tags        = var.kb_tags
+
+  storage_configuration = {
+    type = var.kb_storage_type
+    pinecone_configuration = {
+      connection_string      = var.connection_string
+      credentials_secret_arn = var.credentials_secret_arn
+      field_mapping = {
+        metadata_field = var.metadata_field
+        text_field     = var.text_field
+      }
+      namespace = var.namespace
+    }
+  }
+  knowledge_base_configuration = {
+    type = var.kb_type
+    vector_knowledge_base_configuration = {
+      embedding_model_arn = var.kb_embedding_model_arn
+    }
+  }
+}
+
+# – RDS –
+resource "awscc_bedrock_knowledge_base" "knowledge_base_rds" {
+  count       = var.create_rds_config ? 1 : 0
+  name        = var.kb_name
+  description = var.kb_description
+  role_arn    = var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn
+  tags        = var.kb_tags
+
+  storage_configuration = {
+    type = var.kb_storage_type
+    rds_configuration = {
+      credentials_secret_arn = var.credentials_secret_arn
+      database_name          = var.database_name
+      resource_arn           = var.resource_arn
+      table_name             = var.table_name
+      field_mapping = {
+        metadata_field    = var.metadata_field
+        primary_key_field = var.primary_key_field
+        text_field        = var.text_field
+        vector_field      = var.vector_field
+      }
+    }
+  }
+  knowledge_base_configuration = {
+    type = var.kb_type
+    vector_knowledge_base_configuration = {
+      embedding_model_arn = var.kb_embedding_model_arn
+    }
+  }
+}
+
+# – OpenSearch Serverless Default –
+
+# Create a Collection
+resource "awscc_opensearchserverless_collection" "default_collection" {
+  name        = "default-collection"
+  type        = "VECTORSEARCH"
+  description = "Default collection created by Amazon Bedrock Knowledge base."
+  depends_on = [
+    aws_opensearchserverless_security_policy.security_policy
+  ]
+}
+
+# Encryption Security Policy
+resource "aws_opensearchserverless_security_policy" "security_policy" {
+  name = "awscc-security-policy"
+  type = "encryption"
+  policy = jsonencode({
+    Rules = [
+      {
+        Resource = ["collection/default-collection"]
+        ResourceType = "collection"
+      }
+    ],
+    AWSOwnedKey = true
+  })
+}
+
+# Network policy
+resource "aws_opensearchserverless_security_policy" "nw_policy" {
+  name = "nw-policy"
+  type = "network"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          ResourceType = "collection"
+          Resource = ["collection/default-collection"]
+        },
+        {
+          ResourceType = "dashboard"
+          Resource = ["collection/default-collection"]
+        }
+      ]
+      AllowFromPublic = true
+    }
+  ])
+}
+
+
+# Data policy
+resource "aws_opensearchserverless_access_policy" "hashicorp_kb" {
+  name = "os-access-policy"
+  type = "data"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          ResourceType = "index"
+          Resource = [
+            "index/${awscc_opensearchserverless_collection.default_collection.name}/*"
+          ]
+          Permission = [
+            "aoss:CreateIndex",
+            "aoss:DeleteIndex", # Required for Terraform
+            "aoss:DescribeIndex",
+            "aoss:ReadDocument",
+            "aoss:UpdateIndex",
+            "aoss:WriteDocument"
+          ]
+        },
+        {
+          ResourceType = "collection"
+          Resource = [
+            "collection/${awscc_opensearchserverless_collection.default_collection.name}"
+          ]
+          Permission = [
+            "aoss:CreateCollectionItems",
+            "aoss:DescribeCollectionItems",
+            "aoss:UpdateCollectionItems"
+          ]
+        }
+      ],
+      Principal = [
+        aws_iam_role.bedrock_knowledge_base_role[0].arn,
+        data.aws_caller_identity.current.arn
+      ]
+    }
+  ])
+}
+
+# OpenSearch index
+provider "opensearch" {
+  url         = awscc_opensearchserverless_collection.default_collection.collection_endpoint
+  healthcheck = false
+}
+
+resource "time_sleep" "wait_before_index_creation" {
+  depends_on      = [awscc_opensearchserverless_collection.default_collection]
+  create_duration = "60s" # Wait for 60 seconds before creating the index
+}
+
+resource "opensearch_index" "default_oss_index" {
+  name                           = "bedrock-knowledge-base-default-index"
+  number_of_shards               = "1"
+  number_of_replicas             = "0"
+  index_knn                      = true
+  index_knn_algo_param_ef_search = "512"
+  mappings                       = <<-EOF
+    {
+      "properties": {
+        "bedrock-knowledge-base-default-vector": {
+          "type": "knn_vector",
+          "dimension": 1536,
+          "method": {
+            "name": "hnsw",
+            "engine": "faiss",
+            "parameters": {
+              "m": 16,
+              "ef_construction": 512
+            },
+            "space_type": "l2"
+          }
+        },
+        "AMAZON_BEDROCK_METADATA": {
+          "type": "text",
+          "index": "false"
+        },
+        "AMAZON_BEDROCK_TEXT_CHUNK": {
+          "type": "text",
+          "index": "true"
+        }
+      }
+    }
+  EOF
+  force_destroy                  = true
+  depends_on                     = [time_sleep.wait_before_index_creation]
+}
