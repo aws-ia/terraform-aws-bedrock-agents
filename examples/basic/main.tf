40f9df890ed32aeaf0f6983893352db01b0e8bb9
Alexa Perlov <aperlov@amazon.com>
2024-10-01 17:03:14 -0400

    adding region to providers

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -3,6 +3,19 @@
 # per use-case. The code below should not be copied directly but referenced in order
 # to build your own root module that invokes this module
 #####################################################################################
+variable "region" {
+  type        = string
+  description = "AWS region to deploy the resources"
+  default     = "us-east-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "awscc" {
+  region = var.region
+}
 
 module "terraform-agents" {
   source = "../.." # local example
