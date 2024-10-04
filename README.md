09576c341fdb1c83257e3b17c1d05f64a9d84be3
Manu Chandrasekhar <manuchn@amazon.com>
2024-10-04 13:00:41 -0400

    fix: permissions, index creation delay

--- README.md
+++ README.md
@@ -41,9 +41,10 @@ No modules.
 | [aws_iam_role.agent_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
 | [aws_iam_role.bedrock_knowledge_base_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
 | [aws_iam_role_policy.agent_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_iam_role_policy.bedrock_kb_oss](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
 | [aws_iam_role_policy.kb_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
 | [aws_iam_role_policy_attachment.bedrock_knowledge_base_policy_attachment](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment) | resource |
-| [aws_opensearchserverless_access_policy.hashicorp_kb](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/opensearchserverless_access_policy) | resource |
+| [aws_opensearchserverless_access_policy.data_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/opensearchserverless_access_policy) | resource |
 | [aws_opensearchserverless_security_policy.nw_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/opensearchserverless_security_policy) | resource |
 | [aws_opensearchserverless_security_policy.security_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/opensearchserverless_security_policy) | resource |
 | [awscc_bedrock_agent.bedrock_agent](https://registry.terraform.io/providers/hashicorp/awscc/latest/docs/resources/bedrock_agent) | resource |
---
+++ opensearch.tf
@@ -0,0 +1,139 @@
+# – OpenSearch Serverless Default –
+
+# Create a Collection
+resource "awscc_opensearchserverless_collection" "default_collection" {
+  name        = "default-collection-${random_string.solution_prefix.result}"
+  type        = "VECTORSEARCH"
+  description = "Default collection created by Amazon Bedrock Knowledge base."
+  depends_on = [
+    aws_opensearchserverless_security_policy.security_policy,
+    aws_opensearchserverless_security_policy.nw_policy
+  ]
+}
+
+# Encryption Security Policy
+resource "aws_opensearchserverless_security_policy" "security_policy" {
+  name = "awscc-security-policy-${random_string.solution_prefix.result}"
+  type = "encryption"
+  policy = jsonencode({
+    Rules = [
+      {
+        Resource     = ["collection/default-collection-${random_string.solution_prefix.result}"]
+        ResourceType = "collection"
+      }
+    ],
+    AWSOwnedKey = true
+  })
+}
+
+# Network policy
+resource "aws_opensearchserverless_security_policy" "nw_policy" {
+  name = "nw-policy-${random_string.solution_prefix.result}"
+  type = "network"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          ResourceType = "collection"
+          Resource     = ["collection/default-collection-${random_string.solution_prefix.result}"]
+        },
+        {
+          ResourceType = "dashboard"
+          Resource     = ["collection/default-collection-${random_string.solution_prefix.result}"]
+        }
+      ]
+      AllowFromPublic = true
+    }
+  ])
+}
+
+
+# Data policy
+resource "aws_opensearchserverless_access_policy" "data_policy" {
+  name = "os-access-policy-${random_string.solution_prefix.result}"
+  type = "data"
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          ResourceType = "index"
+          Resource = [
+            "index/${awscc_opensearchserverless_collection.default_collection.name}/*"
+          ]
+          Permission = [
+            "aoss:CreateIndex",
+            "aoss:DeleteIndex", # Required for Terraform
+            "aoss:DescribeIndex",
+            "aoss:ReadDocument",
+            "aoss:UpdateIndex",
+            "aoss:WriteDocument"
+          ]
+        },
+        {
+          ResourceType = "collection"
+          Resource = [
+            "collection/${awscc_opensearchserverless_collection.default_collection.name}"
+          ]
+          Permission = [
+            "aoss:CreateCollectionItems",
+            "aoss:DescribeCollectionItems",
+            "aoss:UpdateCollectionItems",
+            "aoss:DeleteCollectionItems",
+          ]
+        }
+      ],
+      Principal = [
+        var.kb_role_arn != null ? var.kb_role_arn : aws_iam_role.bedrock_knowledge_base_role[0].arn,
+        data.aws_caller_identity.current.arn
+      ]
+    }
+  ])
+}
+
+# OpenSearch index
+
+
+resource "time_sleep" "wait_before_index_creation" {
+  depends_on      = [awscc_opensearchserverless_collection.default_collection]
+  create_duration = "60s" # Wait for 60 seconds before creating the index
+}
+
+resource "opensearch_index" "default_oss_index" {
+  name                           = "bedrock-knowledge-base-default-index-${random_string.solution_prefix.result}"
+  number_of_shards               = "2"
+  number_of_replicas             = "0"
+  index_knn                      = true
+  index_knn_algo_param_ef_search = "512"
+  mappings                       = <<-EOF
+    {
+      "properties": {
+        "bedrock-knowledge-base-default-vector": {
+          "type": "knn_vector",
+          "dimension": 1536,
+          "method": {
+            "name": "hnsw",
+            "engine": "faiss",
+            "parameters": {
+              "m": 16,
+              "ef_construction": 512
+            },
+            "space_type": "l2"
+          }
+        },
+        "AMAZON_BEDROCK_METADATA": {
+          "type": "text",
+          "index": "false"
+        },
+        "AMAZON_BEDROCK_TEXT_CHUNK": {
+          "type": "text",
+          "index": "true"
+        }
+      }
+    }
+  EOF
+  force_destroy                  = true
+  depends_on                     = [time_sleep.wait_before_index_creation]
+  lifecycle {
+    
+  }
+}
