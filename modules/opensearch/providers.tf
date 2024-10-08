394bd8105d664b733c5cb4977c154f45978c2ec2
Alexa Perlov <aperlov@amazon.com>
2024-10-08 15:49:28 -0400

    updating providers

--- modules/opensearch/providers.tf
+++ modules/opensearch/providers.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.0.7"
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>5.0"
+      version = ">=5.8.0"
     }
     awscc = {
       source  = "hashicorp/awscc"
-      version = ">= 1.0.0"
+      version = ">= 0.78.0"
     }
     opensearch = {
       source  = "opensearch-project/opensearch"
@@ -16,10 +16,6 @@ terraform {
     time = {
       source  = "hashicorp/time"
       version = "~> 0.6"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.6.0"
     }
   }
 }
