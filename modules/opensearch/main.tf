29840f5d284b8b971bbacc6f3e99da19f17f5dbd
Alexa Perlov <aperlov@amazon.com>
2024-10-08 16:07:51 -0400

    adding module provider

--- modules/opensearch/main.tf
+++ modules/opensearch/main.tf
@@ -1,8 +1,5 @@
 data "aws_caller_identity" "current" {}
-provider "opensearch" {
-  url         = awscc_opensearchserverless_collection.default_collection.collection_endpoint
-  healthcheck = false
-}
+
 # – OpenSearch Serverless Default –
 
 # Create a Collection
