94d24aed78cd8363f61164349c68e9b5d6489c34
Alexa Perlov <aperlov@amazon.com>
2024-10-14 18:51:22 -0400

    precommit

--- bedrock_kb.tf
+++ bedrock_kb.tf
@@ -10,7 +10,7 @@ resource "awscc_bedrock_knowledge_base" "knowledge_base_default" {
     type = "OPENSEARCH_SERVERLESS"
     opensearch_serverless_configuration = {
       collection_arn    =  awscc_opensearchserverless_collection.default_collection[0].arn
-      vector_index_name = opensearch_index.default_oss_index.name
+      vector_index_name = opensearch_index.default_oss_index[0].name
       field_mapping = {
         metadata_field = var.metadata_field
         text_field     = var.text_field
